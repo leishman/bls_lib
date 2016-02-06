178a7bbea0b4ea2ce88eb5d94747f9fee9ab7beb
Alexander Leishman <leishman3@gmail.com>
2016-02-05 20:02:53 -0800

    get basic validation working

---
+++ include/bls.h
@@ -0,0 +1,14 @@
+
+/*
+ * Dependencies: https://github.com/herumi/ate-pairing
+ */
+
+/*
+ * Function: bls_keygen
+ * Generate a random public/private keypair
+ */
+
+#include <iostream>
+#include "bn.h"
+
+
